0eaf66baf7fdc30b9c328742221683a1ac205e19
Pragatheesvaran A B <95266924+praga-16@users.noreply.github.com>
2026-06-18 14:52:14 +0530

    Remove unnecessary code block formatting

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-```python
+
 import streamlit as st
 from bs4 import BeautifulSoup
 import zipfile
@@ -165,4 +165,4 @@ if uploaded_files:
         file_name="xml_files.zip",
         mime="application/zip"
     )
-```
+
